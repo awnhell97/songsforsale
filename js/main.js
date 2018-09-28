041dbd161e4456df53db58668baef469f8b3d574
Victor Treaba <vtreaba@gmail.com>
2018-09-27 23:59:12 -0400

    bar test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("hey")
+window.addEventListener('load', function() {
+
+    var expanded = false;
+
+    window.addEventListener("scroll", function() {
+        if(!expanded && window.pageYOffset > 0) {
+            expanded = true;
+            document.getElementById("topBar").style.height = "100px";
+        } else if(expanded && window.pageYOffset === 0) {
+            expanded = false;
+            document.getElementById("topBar").style.height = "70px";
+        }
+    });
+
+});
