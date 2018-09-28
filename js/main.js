232aca66ca49b3fcb8d58610b7c0a042e1837505
Victor Treaba <vtreaba@gmail.com>
2018-09-28 01:48:14 -0400

    nice anim vic

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ window.addEventListener('load', function() {
             document.getElementById("topBar").style.height = "50px";
             document.getElementById("barLink").style.lineHeight = "50px";
             document.getElementById("topBar").style.background = "white";
+            document.getElementById("barLink").style.color = "black";
         } else if(expanded && window.pageYOffset === 0) {
             expanded = false;
             document.getElementById("topBar").style.height = "70px";
             document.getElementById("barLink").style.lineHeight = "70px";
             document.getElementById("topBar").style.background = "";
+            document.getElementById("barLink").style.color = "white";
         }
     });
 
